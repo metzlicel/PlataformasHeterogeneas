d442d17b486c59bda69c6b720d1ac94b8b0bd684
metzlicel <110009529+metzlicel@users.noreply.github.com>
2025-09-14 12:21:37 -0700

    Add files via upload

---
+++ ludanortmun cetys-icc-hetplat main 5.DynamicSites-NumericSeries/NumericSeries/Controllers/SeriesController.cs
@@ -0,0 +1,20 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using NumericSeries.Models;
+
+namespace NumericSeries.Controllers
+{
+    public class SeriesController : Controller
+    {
+        [HttpGet("/series/{series}/{n:min(0)}")]
+        public IActionResult Index(string series, int n = 0)
+        {
+            return View(new SeriesViewModel()
+            {
+                Series = series.ApplyCase(LetterCasing.Sentence),
+                N = n,
+                Result = n
+            });
+        }
+    }
+}
