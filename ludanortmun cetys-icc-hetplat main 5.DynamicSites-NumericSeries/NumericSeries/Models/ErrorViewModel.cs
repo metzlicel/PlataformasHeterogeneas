af1cab3bf60b42428a75f3610424f7cadb211dac
metzlicel <110009529+metzlicel@users.noreply.github.com>
2025-09-14 12:22:25 -0700

    Delete ludanortmun cetys-icc-hetplat main 5.DynamicSites-NumericSeries directory

--- ludanortmun cetys-icc-hetplat main 5.DynamicSites-NumericSeries/NumericSeries/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NumericSeries.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- ludanortmun cetys-icc-hetplat main 5.DynamicSites-NumericSeries/NumericSeries/Models/SeriesViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NumericSeries.Models
-{
-    public class SeriesViewModel
-    {
-        public string Series { get; set; } = string.Empty;
-        public int N { get; set; }
-        public long Result { get; set; }
-    }
-}
