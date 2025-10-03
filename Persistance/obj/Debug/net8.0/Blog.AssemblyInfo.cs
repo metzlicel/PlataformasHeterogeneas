00c91b4f62cd25b86f1f33a165b94bfe0f6f630b
metzlicel <110009529+metzlicel@users.noreply.github.com>
2025-10-02 18:34:45 -0700

    Version final

--- Persistance/obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ Persistance/obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006915d2bc2f9be4fbe3684e0e9cd90600305582")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8bd896c2189a7fc0a47699bab0ad13b11528d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
